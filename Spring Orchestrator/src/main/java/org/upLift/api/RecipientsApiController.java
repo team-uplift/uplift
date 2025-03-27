bbe3361cd9acbf74bdf0426587f7fad1cc948782
Christina Molodowitch <37000496+cmolodowitch@users.noreply.github.com>
2025-03-26 22:45:32 -0400

    feat(UA): Remove duplicate endpoints from RecipientsApi
    UserApis: Removed endpoints from RecipientsApi and Controller implementation that duplicate the UsersApi endpoints, since it will now all go through the user endpoints instead.

--- Spring Orchestrator/src/main/java/org/upLift/api/RecipientsApiController.java
+++ Spring Orchestrator/src/main/java/org/upLift/api/RecipientsApiController.java
@@ -1,19 +1,20 @@
 package org.upLift.api;
 
-import org.upLift.model.Recipient;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.upLift.model.Recipient;
 
 import jakarta.validation.Valid;
 import jakarta.servlet.http.HttpServletRequest;
