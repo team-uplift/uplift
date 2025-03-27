a2138fe5d13b166d6e7d02d6664c1042962ca20d
Christina Molodowitch <37000496+cmolodowitch@users.noreply.github.com>
2025-03-26 22:47:46 -0400

    feat(UA): Add DonationRepository

--- Spring Orchestrator/src/main/java/org/upLift/repositories/DonationRepository.java
+++ Spring Orchestrator/src/main/java/org/upLift/repositories/DonationRepository.java
@@ -5,6 +5,20 @@ import org.upLift.model.Donation;
 
 import java.util.List;
 
-public interface DonationRepository extends JpaRepository<Donation, Integer> {
+public interface DonationRepository extends JpaRepository<Donation, Long> {
+
+	/**
+	 * Finds all donations made by the specified donor.
+	 * @param donorId
+	 * @return
+	 */
+	List<Donation> findByDonor_Id(Integer donorId);
+
+	/**
+	 * Finds all donations received by the specified recipient.
+	 * @param recipientId
+	 * @return
+	 */
+	List<Donation> findByRecipient_Id(Integer recipientId);
 
 }
