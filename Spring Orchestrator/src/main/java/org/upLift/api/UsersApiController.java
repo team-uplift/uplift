9505068744ce35e0bca4966a8bfae05280fc7b50
Christina Molodowitch <37000496+cmolodowitch@users.noreply.github.com>
2025-03-29 13:43:32 -0400

    feat(UA): Remove duplicate API doc annotations

--- Spring Orchestrator/src/main/java/org/upLift/api/UsersApiController.java
+++ Spring Orchestrator/src/main/java/org/upLift/api/UsersApiController.java
@@ -1,8 +1,5 @@
 package org.upLift.api;
 
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.enums.ParameterIn;
-import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,14 +24,7 @@ public class UsersApiController implements UsersApi {
 		this.userService = userService;
 	}
 
-	public ResponseEntity<User> addUser(
-			// @formatter:off
-//			@Parameter(in = ParameterIn.HEADER, description = "Tracks the session for the given set of requests.",
-//					required = true,
-//					schema = @Schema()) @RequestHeader(value = "session_id", required = true) String sessionId,
-			// @formatter:on
-			@Parameter(in = ParameterIn.DEFAULT, description = "Create a new user in the app", required = true,
-					schema = @Schema()) @Valid @RequestBody User body) {
+	public ResponseEntity<User> addUser(@Valid @RequestBody User body) {
 		LOG.info("Adding new user: {}", body.isRecipient() ? "recipient" : "donor");
 		LOG.debug("New user info: {}", body);
 		var savedUser = userService.addUser(body);
@@ -42,29 +32,13 @@ public class UsersApiController implements UsersApi {
 	}
 
 	@Override
-	public void deleteUser(
-			// @formatter:off
-//			@Parameter(in = ParameterIn.HEADER, description = "Tracks the session for the given set of requests.",
-//					required = true,
-//					schema = @Schema()) @RequestHeader(value = "session_id", required = true) String sessionId,
-//			@Parameter(in = ParameterIn.HEADER, description = "", schema = @Schema()) @RequestHeader(value = "api_key",
-//							required = false) String apiKey,
-			// @formatter:on
-			@Parameter(in = ParameterIn.PATH, description = "User id to delete", required = true,
-					schema = @Schema()) @PathVariable("userId") Integer userId) {
+	public void deleteUser(@PathVariable("userId") Integer userId) {
 		LOG.info("Deleting user: {}", userId);
 		userService.deleteUser(userId);
 	}
 
 	@Override
-	public ResponseEntity<User> getUserById(
-			// @formatter:off
-//			@Parameter(in = ParameterIn.HEADER, description = "Tracks the session for the given set of requests.",
-//					required = true,
-//					schema = @Schema()) @RequestHeader(value = "session_id", required = true) String sessionId,
-			// @formatter:on
-			@Parameter(in = ParameterIn.PATH, description = "ID of user to return", required = true,
-					schema = @Schema()) @PathVariable("userId") Integer userId) {
+	public ResponseEntity<User> getUserById(@PathVariable("userId") Integer userId) {
 		LOG.info("Retrieving user: {}", userId);
 		var user = userService.getUserById(userId);
 		if (user.isPresent()) {
@@ -76,14 +50,7 @@ public class UsersApiController implements UsersApi {
 	}
 
 	@Override
-	public ResponseEntity<User> getUserByCognitoId(
-			// @formatter:off
-//			@Parameter(in = ParameterIn.HEADER, description = "Tracks the session for the given set of requests.",
-//					required = true,
-//					schema = @Schema()) @RequestHeader(value = "session_id", required = true) String sessionId,
-			// @formatter:on
-			@Parameter(in = ParameterIn.PATH, description = "Cognito ID of user to return", required = true,
-					schema = @Schema()) @PathVariable("cognitoId") String cognitoId) {
+	public ResponseEntity<User> getUserByCognitoId(@PathVariable("cognitoId") String cognitoId) {
 		LOG.info("Retrieving user: {}", cognitoId);
 		var user = userService.getUserByCognitoId(cognitoId);
 		if (user.isPresent()) {
@@ -95,14 +62,7 @@ public class UsersApiController implements UsersApi {
 	}
 
 	@Override
-	public ResponseEntity<User> updateUser(
-			// @formatter:off
-//			@Parameter(in = ParameterIn.HEADER, description = "Tracks the session for the given set of requests.",
-//					required = true,
-//					schema = @Schema()) @RequestHeader(value = "session_id", required = true) String sessionId,
-			// @formatter:on
-			@Parameter(in = ParameterIn.DEFAULT, description = "Update an existent user in the store", required = true,
-					schema = @Schema()) @Valid @RequestBody User body) {
+	public ResponseEntity<User> updateUser(@Valid @RequestBody User body) {
 		LOG.info("Updating user: {}", body.getId());
 		LOG.debug("Updated user info: {}", body);
 		if (userService.userExists(body.getId())) {
