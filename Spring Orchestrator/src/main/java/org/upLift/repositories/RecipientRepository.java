23e0f7d022ae5933dc2ddf3994f49c6b91baa82f
Christina Molodowitch <37000496+cmolodowitch@users.noreply.github.com>
2025-04-02 13:24:18 -0400

    feat(TS): Add repo methods to load recipients by tag
    TagSelection: Added RecipientRepository methods that load recipients based on a tag name.  One method only loads recipients who've selected the tag, and the other loads all recipients linked to the tag (selected or not).

--- Spring Orchestrator/src/main/java/org/upLift/repositories/RecipientRepository.java
+++ Spring Orchestrator/src/main/java/org/upLift/repositories/RecipientRepository.java
@@ -3,6 +3,25 @@ package org.upLift.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.upLift.model.Recipient;
 
+import java.util.List;
+
 public interface RecipientRepository extends JpaRepository<Recipient, Integer> {
+
+	/**
+	 * Loads all recipients that have selected the specified tag.
+	 * @param tag name of the tag which the recipient must have selected
+	 * @return List of recipients who have selected the specified tag or an empty list if
+	 * there are no such recipients
+	 */
+	List<Recipient> findByTags_SelectedIsTrueAndTags_Tag_TagName(String tag);
+
+	/**
+	 * Loads all recipients that are linked to the specified tag, whether they selected it
+	 * or not.
+	 * @param tag name of the tag to which the recipient must be linked (selected or not)
+	 * @return List of recipients who are linked to the specified tag or an empty list if
+	 * there are no such recipients
+	 */
+	List<Recipient> findByTags_Tag_TagName(String tag);
 
 }
