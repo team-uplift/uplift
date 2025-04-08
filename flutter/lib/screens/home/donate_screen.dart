01424bcab6373ce371e66e5fcd7de35f2c283cb6
aaronbwatkins <aaron.watkins@me.com>
2025-04-08 07:14:06 -0700

    added PayPal need keys

--- flutter/lib/screens/home/donate_screen.dart
+++ flutter/lib/screens/home/donate_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_paypal/flutter_paypal.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:uplift/components/standard_button.dart';
 import 'package:uplift/models/recipient_model.dart';
 import 'package:uplift/models/transaction_model.dart';
 import 'package:uplift/providers/transaction_notifier_provider.dart';
@@ -32,6 +34,71 @@ class _DonatePageState extends ConsumerState<DonatePage> {
 
     double parsed = double.parse(value) / 100; // Convert cents to dollars
     return parsed.toStringAsFixed(2);
+  }
+
+  void usePaypal() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (BuildContext context) => UsePaypal(
+            sandboxMode: true,
+            clientId:
+                "AW1TdvpSGbIM5iP4HJNI5TyTmwpY9Gv9dYw8_8yW5lYIbCqf326vrkrp0ce9TAqjEGMHiV3OqJM_aRT0",
+            secretKey:
+                "EHHtTDjnmTZATYBPiGzZC_AZUfMpMAzj2VZUeqlFUrRJA_C0pQNCxDccB5qoRQSEdcOnnKQhycuOWdP9",
+            returnURL: "https://samplesite.com/return",
+            cancelURL: "https://samplesite.com/cancel",
+            transactions: const [
+              {
+                "amount": {
+                  "total": '10.12',
+                  "currency": "USD",
+                  "details": {
+                    "subtotal": '10.12',
+                    "shipping": '0',
+                    "shipping_discount": 0
+                  }
+                },
+                "description": "The payment transaction description.",
+                // "payment_options": {
+                //   "allowed_payment_method":
+                //       "INSTANT_FUNDING_SOURCE"
+                // },
+                "item_list": {
+                  "items": [
+                    {
+                      "name": "A demo product",
+                      "quantity": 1,
+                      "price": '10.12',
+                      "currency": "USD"
+                    }
+                  ],
+
+                  // shipping address is not required though
+                  "shipping_address": {
+                    "recipient_name": "Jane Foster",
+                    "line1": "Travis County",
+                    "line2": "",
+                    "city": "Austin",
+                    "country_code": "US",
+                    "postal_code": "73301",
+                    "phone": "+00000000",
+                    "state": "Texas"
+                  },
+                }
+              }
+            ],
+            note: "Contact us for any questions on your order.",
+            onSuccess: (Map params) async {
+              print("onSuccess: $params");
+            },
+            onError: (error) {
+              print("onError: $error");
+            },
+            onCancel: (params) {
+              print('cancelled: $params');
+            }),
+      ),
+    );
   }
 
   @override
@@ -131,7 +198,7 @@ class _DonatePageState extends ConsumerState<DonatePage> {
                     ref
                         .read(transactionNotifierProvider.notifier)
                         .addTransaction(newTransaction);
-                        context.goNamed('/dashboard');
+                    context.goNamed('/dashboard');
                   },
                   child: const Text(
                     "Donate",
@@ -139,6 +206,11 @@ class _DonatePageState extends ConsumerState<DonatePage> {
                   ),
                 ),
               ),
+              SizedBox(height: 10),
+              StandardButton(
+                title: 'DONATE WITH PAYPAL',
+                onPressed: usePaypal,
+              )
             ],
           ),
         ),
