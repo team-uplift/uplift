ee1be98299b156c95d12e5945de340f6b2db74c8
Christina Molodowitch <37000496+cmolodowitch@users.noreply.github.com>
2025-04-07 22:36:46 -0400

    feat(GRT): Add repository method to get all selected tags
    GetRandomTags: Added TagRepository method to get all recipient-selected tags, for use in generating random list for donors to choose from.

--- Spring Orchestrator/src/main/java/org/upLift/repositories/TagRepository.java
+++ Spring Orchestrator/src/main/java/org/upLift/repositories/TagRepository.java
@@ -1,6 +1,7 @@
 package org.upLift.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.upLift.model.Tag;
 
 import java.util.List;
@@ -8,5 +9,14 @@ import java.util.List;
 public interface TagRepository extends JpaRepository<Tag, Integer> {
 
 	List<Tag> findByTagName(String tagName);
+
+	/**
+	 * Retrieves a list of all tags that have been selected by at least one recipient.
+	 * @return List containing all tags selected by at least one recipient
+	 */
+	@Query("""
+			SELECT tag.tag FROM RecipientTag tag WHERE tag.selected = true GROUP BY tag.tag.tagName
+			""")
+	List<Tag> getSelectedTags();
 
 }
