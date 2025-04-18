3c205eb890793ed0fed3784cca3e038c3cef13f7
Christina Molodowitch <37000496+cmolodowitch@users.noreply.github.com>
2025-04-17 20:48:46 -0400

    feat(AAJ): Switch to using Spring JPA auditing for creation date

--- Spring Orchestrator/src/main/java/org/upLift/UpLiftOrchestrator.java
+++ Spring Orchestrator/src/main/java/org/upLift/UpLiftOrchestrator.java
@@ -8,8 +8,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 @ComponentScan(basePackages = { "org.upLift", "org.upLift.api", "org.upLift.configuration", "org.upLift.services" })
 public class UpLiftOrchestrator implements CommandLineRunner {
 
