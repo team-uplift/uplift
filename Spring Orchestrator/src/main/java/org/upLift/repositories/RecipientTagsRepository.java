6f63c85700b3ca7d477df83b49c550a3c0523d35
Christina Molodowitch <37000496+cmolodowitch@users.noreply.github.com>
2025-04-21 23:31:41 -0400

    feat(FW): Fix RecipientTagsRepository
    FairnessWeights: Fixed RecipientTagsRepository method to use a JPQL query to join fetch both Tag and Recipient parents, to ensure the Recipient parent is correctly loaded.

--- Spring Orchestrator/src/main/java/org/upLift/repositories/RecipientTagsRepository.java
+++ Spring Orchestrator/src/main/java/org/upLift/repositories/RecipientTagsRepository.java
@@ -1,12 +1,18 @@
 package org.upLift.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.upLift.model.RecipientTag;
 
 import java.util.List;
 
 public interface RecipientTagsRepository extends JpaRepository<RecipientTag, Integer> {
 
+	@Query("""
+			SELECT recipientTag FROM RecipientTag recipientTag JOIN FETCH recipientTag.tag
+						JOIN FETCH recipientTag.recipient
+						WHERE recipientTag.tag.tagName = :tagName
+			""")
 	List<RecipientTag> getRecipientTagsByTag_TagName(String tagName);
 
 }
