216bfc4659211a7a9bc53382d1c15a1326c4e974
Christina Molodowitch <37000496+cmolodowitch@users.noreply.github.com>
2025-04-21 23:33:46 -0400

    feat(FW): Add RecipientTagsRepositoryTest

---
+++ Spring Orchestrator/src/test/java/org/upLift/repositories/RecipientTagsRepositoryTest.java
@@ -0,0 +1,55 @@
+package org.upLift.repositories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.upLift.model.RecipientTag;
+
+import java.time.Instant;
+import java.util.Comparator;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+class RecipientTagsRepositoryTest extends BaseRepositoryTest {
+
+	@Autowired
+	private RecipientTagsRepository repository;
+
+	@Test
+	void findById() {
+		var result = repository.findById(5);
+		assertThat(result.isPresent(), is(true));
+		checkTag(result.get());
+	}
+
+	@Test
+	void getRecipientTagsByTagName() {
+		var result = repository.getRecipientTagsByTagName("health");
+		assertThat(result, hasSize(4));
+		result.sort(Comparator.comparing(RecipientTag::getId));
+
+		var tag1 = result.get(0);
+		checkTag(tag1);
+
+		var tag2 = result.get(1);
+		assertThat(tag2.getId(), is(10));
+		assertThat(tag2.getTag(), is(notNullValue()));
+		assertThat(tag2.getTagName(), is("health"));
+		assertThat(tag2.getRecipient(), is(notNullValue()));
+		assertThat(tag2.getRecipient().getId(), is(2));
+
+		assertThat(result.get(2).getId(), is(18));
+		assertThat(result.get(3).getId(), is(26));
+	}
+
+	void checkTag(RecipientTag tag) {
+		assertThat(tag.getId(), is(5));
+		assertThat(tag.getTag(), is(notNullValue()));
+		assertThat(tag.getTagName(), is("health"));
+		assertThat(tag.getRecipient(), is(notNullValue()));
+		assertThat(tag.getRecipient().getId(), is(1));
+		assertThat(tag.getWeight(), is(0.8));
+		assertThat(tag.isSelected(), is(true));
+		assertThat(tag.getAddedAt(), is(Instant.parse("2023-10-05T09:30:30.123Z")));
+	}
+}
