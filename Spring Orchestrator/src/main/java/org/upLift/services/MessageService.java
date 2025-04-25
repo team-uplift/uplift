d78f0cd0a3131519e8933c3bfcfb6d5acbf2e2d9
Christina Molodowitch <37000496+cmolodowitch@users.noreply.github.com>
2025-04-24 20:18:23 -0400

    fix(M): Update MessageService new message/update handling
    Messages: Rename MessageService method `sendMessage()` to `sendNewMessage()` and expanded functionality to add validation checks on donation and existing messages.  Added new MessageService method `markMessageRead()` to separate a simple update of the flag from the logic needed when adding a new message.

--- Spring Orchestrator/src/main/java/org/upLift/services/MessageService.java
+++ Spring Orchestrator/src/main/java/org/upLift/services/MessageService.java
@@ -13,6 +13,21 @@ public interface MessageService {
 
 	List<Message> getMessagesByDonorId(int donorId);
 
-	Message sendMessage(Message message);
+	/**
+	 * Saves the specified new message, returning the donation to which it's attached.
+	 * Method returns the parent Donation to facilitate use by the front end, since
+	 * recipient-sent messages are only displayed in the context of a donation.
+	 * @param message new thank-you message to be saved, must include the parent donation id
+	 * @return new saved Message entry
+	 */
+	Message sendNewMessage(Message message);
+
+	/**
+	 * Marks the message with the specified persistence id as read by the donor, returning
+	 * the updated message.
+	 * @param id persistence index of the message to mark as having been read by the donor
+	 * @return updated Message entry
+	 */
+	Message markMessageRead(int id);
 
 }
