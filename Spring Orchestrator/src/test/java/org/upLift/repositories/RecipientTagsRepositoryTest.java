5c9c1c226dc8ac32d3ff7e8ed6c5117bdb0ed6af
Christina Molodowitch <37000496+cmolodowitch@users.noreply.github.com>
2025-04-29 12:27:17 -0400

    feat(DU): Update test for deleted recipient filtering

--- Spring Orchestrator/src/test/java/org/upLift/repositories/RecipientTagsRepositoryTest.java
+++ Spring Orchestrator/src/test/java/org/upLift/repositories/RecipientTagsRepositoryTest.java
@@ -24,22 +24,34 @@ class RecipientTagsRepositoryTest extends BaseRepositoryTest {
 
 	@Test
 	void getRecipientTagsByTagName() {
+		// Deleted recipient 9 is linked to the "health" tag
+		var deletedRecipientTag = repository.findById(36)
+			.orElseThrow(() -> new RuntimeException("No recipient tag found"));
+		assertThat(deletedRecipientTag.getTagName(), is("health"));
+		assertThat(deletedRecipientTag.getRecipient().getId(), is(9));
+
 		var result = repository.getRecipientTagsByTagName("health");
+
+		// Result should not include the recipient tag for the deleted recipient
 		assertThat(result, hasSize(4));
 		result.sort(Comparator.comparing(RecipientTag::getId));
 
-		var tag1 = result.get(0);
+		var tag1 = result.getFirst();
 		checkTag(tag1);
+
+		for (RecipientTag tag : result) {
+			assertThat(tag.getTagName(), is("health"));
+		}
 
 		var tag2 = result.get(1);
 		assertThat(tag2.getId(), is(10));
-		assertThat(tag2.getTag(), is(notNullValue()));
-		assertThat(tag2.getTagName(), is("health"));
 		assertThat(tag2.getRecipient(), is(notNullValue()));
 		assertThat(tag2.getRecipient().getId(), is(2));
 
 		assertThat(result.get(2).getId(), is(18));
+		assertThat(result.get(2).getRecipient().getId(), is(5));
 		assertThat(result.get(3).getId(), is(26));
+		assertThat(result.get(3).getRecipient().getId(), is(6));
 	}
 
 	void checkTag(RecipientTag tag) {
