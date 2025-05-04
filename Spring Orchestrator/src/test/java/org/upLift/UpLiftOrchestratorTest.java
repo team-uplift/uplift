15df86e7e5c4b9003427f76729865ffd25c6906e
Christina Molodowitch <37000496+cmolodowitch@users.noreply.github.com>
2025-05-03 20:51:46 -0400

    feat(CUT): Add HomeController to UpLiftOrchestratorTest

--- Spring Orchestrator/src/test/java/org/upLift/UpLiftOrchestratorTest.java
+++ Spring Orchestrator/src/test/java/org/upLift/UpLiftOrchestratorTest.java
@@ -3,20 +3,23 @@ package org.upLift;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.upLift.api.DonationsApiController;
-import org.upLift.api.MessagesApiController;
-import org.upLift.api.RecipientsApiController;
-import org.upLift.api.UsersApiController;
+import org.upLift.api.*;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 
+/**
+ * This serves as a "smoke" test for the configuration, to make sure that the system starts up correctly.
+ */
 @SpringBootTest
 class UpLiftOrchestratorTest {
 
 	@Autowired
 	private DonationsApiController donationsApiController;
+
+	@Autowired
+	private HomeController homeController;
 
 	@Autowired
 	private MessagesApiController messagesApiController;
@@ -30,6 +33,7 @@ class UpLiftOrchestratorTest {
 	@Test
 	void contextLoads() {
 		assertThat(donationsApiController, is(notNullValue()));
+		assertThat(homeController, is(notNullValue()));
 		assertThat(messagesApiController, is(notNullValue()));
 		assertThat(recipientsApiController, is(notNullValue()));
 		assertThat(usersApiController, is(notNullValue()));
